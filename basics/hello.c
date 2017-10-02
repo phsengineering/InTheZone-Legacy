1ad8e49af749fe0b7eb5b6614700e59d1a15f415
javathunderman <javathunderman@users.noreply.github.com>
2017-10-02 16:10:55 -0400

    Basic introduction to ROBOTC
    Enable motor 2 and 3 to run at 63 (half power) for three seconds before ending program.

---
+++ basics/hello.c
@@ -0,0 +1,11 @@
+/*
+Multi-line comments at the top of programs are often 
+used to explain what the entire program will do.
+*/
+
+task main()
+{
+   motor[port2] = 63;
+   motor[port3] = 63;
+   wait1Msec(3000);
+}
